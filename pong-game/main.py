217c3b4de0360c93c39c80353e8d3866cae98a22
z6wdc <whitedeathcross@gmail.com>
2021-01-31 22:00:49 +0900

    exercise: pong game

---
+++ pong-game/main.py
@@ -0,0 +1,51 @@
+from ball import Ball
+from turtle import Screen
+from paddle import Paddle
+from scoreboard import ScoreBoard
+import time
+
+BALL_SPEED = 0.1
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle(paddle_x=350, paddle_y=0)
+l_paddle = Paddle(paddle_x=-350, paddle_y=0)
+ball = Ball()
+scoreboard = ScoreBoard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
+is_game_on = True
+
+while is_game_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.xcor() > 380:
+        scoreboard.r_point()
+        ball.reset_position()
+
+    if ball.xcor() < -380:
+        scoreboard.l_point()
+        ball.reset_position()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320:
+        ball.bounce_x()
+
+    if ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+screen.exitonclick()
